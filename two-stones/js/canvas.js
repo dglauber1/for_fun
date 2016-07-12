de58095a06c5c2c8844cc2aa56087a639ff406a8
Daniel Glauber <daniel_glauber@brown.edu>
2016-07-12 13:25:21 -0400

    ui and ai need work, but game is functional now

--- two-stones/js/canvas.js
+++ two-stones/js/canvas.js
@@ -30,23 +30,117 @@ mousedown = false;
 mapdragged = false;
 mouse_x = 0;
 mouse_y = 0;
+humanturn = 1;
+longeststreak = 0;
+
+function getstreak(x, y) {
+    var toReturn = {};
+    var maxStreak = 0;
+    var toPlaceX;
+    var toPlaceY;
+    for (var i = -1; i < 2; i++) {
+        for (var j = -1; j < 2; j++) {
+            if (pieces[i + x] == null || pieces[i + x][j + y] == null) {
+                incrementX = i * -1;
+                incrementY = j * -1;
+                var streak = 1;
+                while (pieces[incrementX + x] != null && pieces[incrementX + x][incrementY + y] != null && pieces[incrementX + x][incrementY + y] != -1) {
+                    streak++;
+                    incrementX += i * -1;
+                    incrementY += j * -1;    
+                }
+                if (streak > maxStreak) {
+                    maxStreak = streak;
+                    toPlaceX = i + x;
+                    toPlaceY = j + y;
+                }
+            }
+        }
+    }
+    toReturn["streak"] = maxStreak;
+    toReturn["toPlaceX"] = toPlaceX;
+    toReturn["toPlaceY"] = toPlaceY;
+    return toReturn;
+}
+
+function computerPlacePiece() {
+    var gridX;
+    var gridY;
+    var maxStreak = 0;
+    for (var x in pieces) {
+        for (var y in pieces[x]) {
+            if (pieces[x][y] == -1) {
+                continue;
+            }
+            var returnObject = getstreak(parseInt(x), parseInt(y));
+            if (returnObject["streak"] > maxStreak) {
+                gridX = returnObject["toPlaceX"];
+                gridY = returnObject["toPlaceY"];
+                maxStreak = returnObject["streak"];
+            }
+        }
+    }
+    if (pieces[gridX] == null) {
+        pieces[gridX] = {};
+    }
+    pieces[gridX][gridY] = -1;
+}
+
+function updateStreak(x, y) {
+    var maxStreak = 0;
+    for (var i = -1; i < 2; i++) {
+        for (var j = -1; j < 2; j++) {
+            if (pieces[i + x] == null || pieces[i + x][j + y] == null) {
+                incrementX = i * -1;
+                incrementY = j * -1;
+                var streak = 1;
+                while (pieces[incrementX + x] != null && pieces[incrementX + x][incrementY + y] != null && pieces[incrementX + x][incrementY + y] != -1) {
+                    streak++;
+                    incrementX += i * -1;
+                    incrementY += j * -1;    
+                }
+                if (streak > maxStreak) {
+                    maxStreak = streak;
+                }
+            }
+        }
+    }
+    if (maxStreak < longeststreak) {
+        return;
+    }
+    longeststreak = maxStreak;
+    streakString = "";
+    for (var i = 0; i < maxStreak; i++) {
+        streakString += "&#9679;";
+    }
+    $("#streak").html(streakString);
+}
 
 function handleMouseClick() {
 	if (computerthinking) {
 		return;
 	}
+    humanturn++;
 	var gridX = Math.floor((mouse_x + CANVAS_X) / PIXELS_PER_TILE); 
 	var gridY = Math.floor((mouse_y + CANVAS_Y) / PIXELS_PER_TILE);
-	if (pieces[gridX] != null && pieces[gridX][gridY] != null) {
+    if (pieces[gridX] != null && pieces[gridX][gridY] != null) {
 		return;
 	}
 	if (pieces[gridX] == null) {
 		pieces[gridX] = {};
 	}
 	pieces[gridX][gridY] = 1;
+    updateStreak(gridX, gridY);
 	paintMap();
-	console.log(gridX);
-	console.log(gridY);
+    if (humanturn > 2) {
+        computerthinking = true;
+        computerPlacePiece();
+        setTimeout(function() { 
+            paintMap();
+            computerthinking = false;
+        }, 500);  
+        humanturn = 1;
+    }
 }
 
 
--- two-stones/js/paint.js
+++ two-stones/js/paint.js
@@ -1,8 +1,8 @@
 function paintMap() {
     var ctx = map.getContext('2d');
     ctx.clearRect(0, 0, map.width, map.height);
-	var topLeftX = Math.ceil(CANVAS_X / PIXELS_PER_TILE) - 1; //grid coordinate of top left tile
-	var topLeftY = Math.ceil(CANVAS_Y / PIXELS_PER_TILE) - 1; //grid coordinate of top left tile
+	var topLeftX = Math.floor(CANVAS_X / PIXELS_PER_TILE); //grid coordinate of top left tile
+	var topLeftY = Math.floor(CANVAS_Y / PIXELS_PER_TILE); //grid coordinate of top left tile
 	for (var i = topLeftX; i < topLeftX + GRID_WIDTH + 1; i++) {
 		if (pieces[i] == null) {
 			continue;
@@ -12,10 +12,18 @@ function paintMap() {
                 continue;
             } else {
 				ctx.beginPath();
-				ctx.rect(((-1 * CANVAS_X) % PIXELS_PER_TILE - PIXELS_PER_TILE) % PIXELS_PER_TILE + (i - topLeftX) * PIXELS_PER_TILE, 
-						 ((-1 * CANVAS_Y) % PIXELS_PER_TILE - PIXELS_PER_TILE) % PIXELS_PER_TILE + (j - topLeftY) * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE);
-				ctx.fillStyle = 'black';
+				ctx.arc(((-1 * CANVAS_X) % PIXELS_PER_TILE - PIXELS_PER_TILE) % PIXELS_PER_TILE + (i - topLeftX) * PIXELS_PER_TILE + 0.5 * PIXELS_PER_TILE, 
+						((-1 * CANVAS_Y) % PIXELS_PER_TILE - PIXELS_PER_TILE) % PIXELS_PER_TILE + (j - topLeftY) * PIXELS_PER_TILE + 0.5 * PIXELS_PER_TILE, 
+                        PIXELS_PER_TILE * 0.8 / 2, 0, 2 * Math.PI);
+				if (pieces[i][j] == 1) {
+                    ctx.fillStyle = 'black';
+                } else {
+                    ctx.fillStyle = 'white';
+                }
 				ctx.fill();
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 3;
+                ctx.stroke();
 				ctx.closePath();
             }
         }
